9f9585ffb791798b86aecf24d8cd8c5e9f47d388
Michael Hastings <mhastin1@gmail.com>
2022-11-22 04:02:18 -0500

    Final version of interest calulator

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
+//Populate the variables from the data that was input on the form
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+//Test for valid principal amount
+    if (principal <= 0){
+        alert("Please enter a positve number");
+        document.getElementById("principal").focus();
+    }
+//If principal value is valid then show the output
+    else{
+        document.getElementById("result").innerHTML="If you deposit <mark>" +principal+ "</mark><br/>"+
+                                                    "at an interest rate of <mark>" +rate+ "%</mark><br/>"+
+                                                    "you will recieve an amount of <mark>" +interest+ "</mark><br/>"+
+                                                    "in the year <mark>" +year+ "</mark><br/>";
+    }
     
 }
         
